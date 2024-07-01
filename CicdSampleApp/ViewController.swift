d531f3b409c0cc3a7fe3a495028229aceec35659
ShiwaniSharma1108 <shiwani.sharma@perennialsys.com>
2024-07-01 12:56:50 +0530

    check for failed case

--- CicdSampleApp/ViewController.swift
+++ CicdSampleApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         self.view.backgroundColor = .yellow
         printSomething()
     }
+    rcfsedrfcsdfvcsdfvcsd
     
     private func printSomething(){
         print("hello github actions")
